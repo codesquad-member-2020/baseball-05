7092eb8c28e8122ac81f9c9cd628f294c291e143
ehgud0670 <kimdo2297@gmail.com>
2020-05-04 18:27:10 +0900

    [iOS] chore: fix untracked files

---
+++ iOS/BaseballGame/BaseballGameTests/BaseballGameTests.swift
@@ -0,0 +1,34 @@
+//
+//  BaseballGameTests.swift
+//  BaseballGameTests
+//
+//  Created by kimdo2297 on 2020/05/04.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import XCTest
+@testable import BaseballGame
+
+class BaseballGameTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
