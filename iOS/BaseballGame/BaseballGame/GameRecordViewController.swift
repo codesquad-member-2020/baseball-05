1f35461f6bbb91da4e45d96f7580cef74c03fd8a
ehgud0670 <kimdo2297@gmail.com>
2020-05-04 21:47:04 +0900

    [#4] Refactor: 게임 스택뷰가 제네릭을 써서인지 추가하면 런타임에러가 나서 삭제함

--- iOS/BaseballGame/BaseballGame/GameRecordViewController.swift
+++ iOS/BaseballGame/BaseballGame/GameRecordViewController.swift
@@ -12,25 +12,8 @@ final class GameRecordViewController: UIViewController {
     //MARK:- IBOutlet properties
     @IBOutlet weak var gameListLabel: TitleLabel!
     
-    //MARK:- Internal properties
-    private let gameInfoStackView = GameInfoStackView()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureGameInfoStackView()
-    }
-    
-    private func configureGameInfoStackView() {
-        view.addSubview(gameInfoStackView)
-        gameInfoStackView.topAnchor.constraint(equalTo: gameListLabel.bottomAnchor,
-                                               constant: 26).isActive  = true
-        let safeArea = view.safeAreaLayoutGuide
-        gameInfoStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor,
-                                                   constant: 10).isActive = true
-        gameInfoStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor,
-                                                    constant: -10).isActive = true
-        gameInfoStackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor,
-                                                  constant: -10).isActive = true
     }
 }
 
