1b705c8a031c2831a14a6e4c633a169ef2021c36
ever <hsik0225@gmail.com>
2020-05-05 15:25:15 +0900

    [#8] feat : 프로젝트 빌드

---
+++ BE/src/main/java/com/codesquad/baseball05/Baseball05Application.java
@@ -0,0 +1,13 @@
+package com.codesquad.baseball05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baseball05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Baseball05Application.class, args);
+    }
+
+}
---
+++ BE/src/test/java/com/codesquad/baseball05/Baseball05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad.baseball05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baseball05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
