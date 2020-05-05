c1956d0adf91b71544be49dd4c6a41162341365e
ever <hsik0225@gmail.com>
2020-05-05 16:16:33 +0900

    [#8] feat : Mock Test

--- BE/src/main/java/com/codesquad/baseball05/ui/MockupController.java
+++ BE/src/main/java/com/codesquad/baseball05/ui/MockupController.java
@@ -1,5 +1,21 @@
 package com.codesquad.baseball05.ui;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class MockupController {
 
+    @GetMapping("/")
+    public Object test() {
+        List<Test> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Test test = new Test("시험" + i);
+            list.add(test);
+        }
+        return list;
+    }
 }
---
+++ BE/src/main/java/com/codesquad/baseball05/ui/Test.java
@@ -0,0 +1,10 @@
+package com.codesquad.baseball05.ui;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Test {
+    String test;
+}
