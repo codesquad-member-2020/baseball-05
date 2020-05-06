8cefc168cee90774b7c3a7209de5cc6890a006c6
Hu2y <sanghun_@daum.net>
2020-05-06 14:31:55 +0900

    [#13] Feat: 게임 진행화면 View 구성, Score 컴포넌트 생성
     - 로그인 페이지 스켈레톤 컴포넌트 생성

---
+++ FE/src/components/MainScoreComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <h2>BASEBALL GAME ONLINE</h2>
+    <div class="match-info-container">
+      <span class="home-team-title">
+        {{ matchArr.homeTeam.name }}
+      </span>
+      <span class="home-team-score">
+        {{ matchArr.homeTeam.score }}
+      </span>
+      vs
+      <span class="away-team-score">
+        {{ matchArr.awayTeam.score }}
+      </span>
+      <span class="away-team-title">
+        {{ matchArr.awayTeam.name }}
+      </span>
+    </div>
+    <div v-if="matchArr.homeTeam.isUserTeam">
+      A가 우리팀
+    </div>
+    <div v-else>
+      B가 우리팀
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(['matchArr']),
+  },
+};
+</script>
+
+<style></style>
