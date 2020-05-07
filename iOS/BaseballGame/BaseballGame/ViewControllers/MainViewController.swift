73c02d984a8d70d8f5a42ccaf324aa8fe94d68de
ehgud0670 <kimdo2297@gmail.com>
2020-05-07 23:39:37 +0900

    [#32] feat: 메인 화면의 이미지와 타이틀 추가 및 오토레이아웃 적용

---
+++ iOS/BaseballGame/BaseballGame/ViewControllers/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  BaseballGame
+//
+//  Created by kimdo2297 on 2020/05/07.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
