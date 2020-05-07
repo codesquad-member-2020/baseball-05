06a057c1e670c8b9ced367620ca7b8e6d97798de
Hu2y <sanghun_@daum.net>
2020-05-07 13:47:39 +0900

    [#17] Feat: 하드코딩으로 랜더링 스켈레톤
     - mockData가 나오면 랜더링작업을 다시 해주려고 한다.

---
+++ FE/src/components/GameBoardComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="batter-info">
+      <span>S <span class="strike-number">🟢🟢</span></span>
+      <span>B <span class="strike-number">🟡🟡🟡</span></span>
+      <span>O <span class="strike-number">🔴🔴</span></span>
+    </div>
+    <div class="base-info"></div>
+    <div class="inning-info">
+      <span>{{ matchArr.inningMeaage }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+  computed: {
+    ...mapState(['matchArr']),
+    countImage: () => {},
+  },
+};
+</script>
+
+<style></style>
