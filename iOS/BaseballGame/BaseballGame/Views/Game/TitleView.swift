fd5031a65575bad46661ca3521f17956c68c6629
ehgud0670 <kimdo2297@gmail.com>
2020-05-09 12:30:29 +0900

    [#35] feat: custom view인 title view를 만듬 	    이후 IBOutlet 프로퍼티를 추가할 예정이다.

---
+++ iOS/BaseballGame/BaseballGame/Views/Game/TitleView.swift
@@ -0,0 +1,19 @@
+//
+//  TitleView.swift
+//  BaseballGame
+//
+//  Created by kimdo2297 on 2020/05/09.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class TitleView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
