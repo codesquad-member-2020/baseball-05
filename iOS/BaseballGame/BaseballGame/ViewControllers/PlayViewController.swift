c08b98deb5c0dbbe924eff934c772333758542ff
ehgud0670 <kimdo2297@gmail.com>
2020-05-09 13:12:45 +0900

    [#35] feat: 게임 play 하는 뷰컨트롤러와 게임 score를 보여주는 뷰 컨트롤러를 	일단 생성시키고 storyboard에 추가함

---
+++ iOS/BaseballGame/BaseballGame/ViewControllers/PlayViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlayViewController.swift
+//  BaseballGame
+//
+//  Created by kimdo2297 on 2020/05/09.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ iOS/BaseballGame/BaseballGame/ViewControllers/ScoresViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ScoresViewController.swift
+//  BaseballGame
+//
+//  Created by kimdo2297 on 2020/05/09.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class ScoresViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
