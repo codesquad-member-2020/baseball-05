21f07e6f657749115b2766919ed55fb74a15e430
ehgud0670 <kimdo2297@gmail.com>
2020-05-09 18:28:19 +0900

    [#35] feat: 점수판에 대한 뷰와 xib 파일 생성함 	    가운데 레이블은 VersusLabel 타입이고, 나머지 모두 TitleLabel 타입이다

---
+++ iOS/BaseballGame/BaseballGame/Views/Game/ScoreView.swift
@@ -0,0 +1,13 @@
+//
+//  ScoreView.swift
+//  BaseballGame
+//
+//  Created by kimdo2297 on 2020/05/09.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class ScoreView: UIView {
+    
+}
