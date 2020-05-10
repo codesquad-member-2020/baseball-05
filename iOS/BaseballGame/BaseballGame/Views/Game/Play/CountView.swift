7e72fe04a33de51515184513c30103125419ce77
ehgud0670 <kimdo2297@gmail.com>
2020-05-10 13:58:43 +0900

    [#5] feat: CountView에 대한 뷰와 xib 을 추가하고 스토리보드에서 사용함 	xib를 쓴 이유: 해당 뷰를 재사용하기 위해 사용하였다.

---
+++ iOS/BaseballGame/BaseballGame/Views/Game/Play/CountView.swift
@@ -0,0 +1,22 @@
+//
+//  CountView.swift
+//  BaseballGame
+//
+//  Created by kimdo2297 on 2020/05/10.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+final class CountView: UIView, WithXib {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        insertXibView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        insertXibView()
+    }
+}
