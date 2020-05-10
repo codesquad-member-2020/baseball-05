0a05ef8dbd53cf5284a9166a06cb9c2daed84cf8
ehgud0670 <kimdo2297@gmail.com>
2020-05-10 18:00:48 +0900

    [#5] feat: 타자의 정보를 보여주는 cell을 추가함

---
+++ iOS/BaseballGame/BaseballGame/Views/Game/Play/BatterInfoCell.swift
@@ -0,0 +1,27 @@
+//
+//  BatterInfoCell.swift
+//  BaseballGame
+//
+//  Created by kimdo2297 on 2020/05/10.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class BatterInfoCell: UITableViewCell, IdentifiableView {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
