942af3275d892d1e291fa4bcf3b6a2d9391bcfc3
Hu2y <sanghun_@daum.net>
2020-05-10 14:27:02 +0900

    [#40] Chore: 로그인 View쪽에 일단 선수명단 컴포넌트 랜더링 확인

--- FE/src/views/LoginView.vue
+++ FE/src/views/LoginView.vue
@@ -2,14 +2,17 @@
   <div>
     login Page
     <score-info-component></score-info-component>
+    <player-info-component></player-info-component>
   </div>
 </template>
 
 <script>
 import ScoreInfoComponent from '@/components/ScoreInfoComponent';
+import PlayerInfoComponent from '@/components/PlayerInfoComponent';
 export default {
   components: {
     ScoreInfoComponent,
+    PlayerInfoComponent,
   },
 };
 </script>
