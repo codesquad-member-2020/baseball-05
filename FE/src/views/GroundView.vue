39a9c0a36993335d9b7aec2396fe81e2dafd5c3e
Hu2y <sanghun_@daum.net>
2020-05-11 22:13:25 +0900

    [#43] Feat: Game진행 View에 컴포넌트연결

--- FE/src/views/GroundView.vue
+++ FE/src/views/GroundView.vue
@@ -2,17 +2,32 @@
   <div class="ground-container">
     <ground-component></ground-component>
     <play-button></play-button>
+    <game-log-component></game-log-component>
+    <score-info-component></score-info-component>
+    <player-info-component></player-info-component>
+    <player-info-button></player-info-button>
+    <score-info-button></score-info-button>
   </div>
 </template>
 
 <script>
 import GroundComponent from '@/components/GroundComponent';
 import PlayButton from '@/components/PlayButton';
+import GameLogComponent from '@/components/Game/GameLogComponent';
+import ScoreInfoComponent from '@/components/StatusBoard/ScoreInfoComponent';
+import PlayerInfoComponent from '@/components/StatusBoard/PlayerInfoComponent';
+import PlayerInfoButton from '@/components/PlayerInfoButton';
+import ScoreInfoButton from '@/components/ScoreInfoButton';
 
 export default {
   components: {
     GroundComponent,
     PlayButton,
+    GameLogComponent,
+    ScoreInfoComponent,
+    PlayerInfoComponent,
+    PlayerInfoButton,
+    ScoreInfoButton,
   },
 };
 </script>
