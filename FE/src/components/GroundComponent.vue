3133722b7e81378bb55369d8dbb659b74928ea2c
Hu2y <sanghun_@daum.net>
2020-05-11 15:43:18 +0900

    [#43] Ground Component 생성
     - 해당컴포넌트는 vuex에서 베이스 데이터를 받아와서 플레이어를 추가해주는 컴포넌트이다
     - 베이스 데이터를 player컴포넌트 props로 내려주고있다
     - 애니메이션이 완료되었을때 다음 타자가 타석에 들어오도록 currentBatter변수를 만들어주었다
     - 일단은 하드코딩으로 버튼을 클릭할시 타석 상태값을 토글 시켜주려고 한다

---
+++ FE/src/components/GroundComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="ground">
+    <player :class="{ active: currentBatter }" />
+    <div class="ground-wrap" v-for="player in players" :key="player.index">
+      <player :class="player" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Player from '@/components/Player';
+
+export default {
+  data() {
+    return {
+      players: ['first', 'second', 'third'],
+      currentBatter: true,
+    };
+  },
+  components: {
+    Player,
+  },
+};
+</script>
+
+<style>
+.ground {
+  background-color: #000;
+}
+
+.active {
+  display: none;
+}
+</style>
