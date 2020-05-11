f8dafcc12eb1d1610dc1d4b0fa54f9dbfd23f4ec
Hu2y <sanghun_@daum.net>
2020-05-11 15:45:00 +0900

    [#43] GroundView 파일 생성
     - 기획서와 다르게 레이아웃을 하고싶어서 새로 만든 컴포넌트이다
     - 해당컴포넌트에 경기 진행에 필요한 컴포넌트들을 붙여줄 계획이다

---
+++ FE/src/views/GroundView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="ground-container">
+    <ground-component></ground-component>
+    <play-button></play-button>
+  </div>
+</template>
+
+<script>
+import GroundComponent from '@/components/GroundComponent';
+import PlayButton from '@/components/PlayButton';
+
+export default {
+  components: {
+    GroundComponent,
+    PlayButton,
+  },
+};
+</script>
+
+<style>
+#app {
+  background: #000;
+}
+
+.ground-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 1200px;
+  height: 700px;
+  background: url('https://s3-us-west-2.amazonaws.com/mfbrowndesign.com/img/baseball+diamond.jpg')
+    no-repeat;
+  background-size: 100% 100%;
+}
+</style>
