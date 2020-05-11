f0945753f3cf89bf414d80e3cdc897519956c4af
Hu2y <sanghun_@daum.net>
2020-05-11 15:45:37 +0900

    [#43] playButton 컴포넌트 생성
     - 경기진행에 필요한 버튼컴포넌트를 생성해주었다
     - 버튼을 클릭할떄마다 상태값을 바꿔주려고 한다

---
+++ FE/src/components/PlayButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button class="playBtn" @click="throwBall"></button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['currentBatter', 'basePlayers']),
+  },
+  methods: {
+    throwBall() {
+      console.log('throw');
+    },
+  },
+};
+</script>
+
+<style>
+.playBtn {
+  position: absolute;
+  bottom: 220px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 50px;
+  height: 60px;
+  background: url('../assets/pitcher.png') no-repeat;
+  background-size: 100% 100%;
+}
+</style>
