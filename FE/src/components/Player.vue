605df8e1c0b34af54c117471d9b4e763bf648964
Hu2y <sanghun_@daum.net>
2020-05-11 15:40:36 +0900

    [#43] Player 컴포넌트 생성
     - player 컴포넌트는 베이스 를 밟고 있는 player를 말한다
     - 부모 컴포넌트에서 props로 현재 베이스 위치를 내려준다
     - 현재 베이스위치를 받아서 키프레임으로 랜더링 시켜주고있다

---
+++ FE/src/components/Player.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="playerImg" :class="propsData" />
+</template>
+
+<script>
+export default {
+  class: ['propsData'],
+};
+</script>
+
+<style>
+.playerImg {
+  position: absolute;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 40px;
+  height: 60px;
+  background: url('../assets/12.png') no-repeat;
+  background-size: 100% 100%;
+  animation-duration: 2s;
+  animation-fill-mode: both;
+  animation-timing-function: cubic-bezier(0.54, -0.39, 0.8, 1.72);
+}
+
+.first {
+  animation-name: first;
+}
+.second {
+  animation-name: second;
+}
+.third {
+  animation-name: third;
+}
+.home {
+  animation-name: home;
+}
+@keyframes first {
+  0% {
+    bottom: 30px;
+    left: 50%;
+  }
+  99% {
+    transform: translateX(-50%) rotate(3600deg) scaleX(1);
+    bottom: 220px;
+    left: 72%;
+  }
+  100% {
+    transform: translateX(-50%) scaleX(-1);
+    bottom: 220px;
+    left: 72%;
+  }
+}
+@keyframes second {
+  0% {
+    bottom: 220px;
+    left: 72%;
+  }
+  99% {
+    transform: translateX(-50%) rotate(3600deg) scaleX(1);
+    bottom: 330px;
+    left: 50%;
+  }
+  100% {
+    transform: translateX(-50%) scaleX(-1);
+    bottom: 330px;
+    left: 50%;
+  }
+}
+@keyframes third {
+  0% {
+    bottom: 330px;
+    left: 50%;
+  }
+  99% {
+    transform: translateX(-50%) rotate(3600deg) scaleX(1);
+    bottom: 220px;
+    left: 28%;
+  }
+  100% {
+    transform: translateX(-50%) scaleX(1);
+    bottom: 220px;
+    left: 28%;
+  }
+}
+@keyframes home {
+  0% {
+    bottom: 220px;
+    left: 28%;
+  }
+  99% {
+    transform: translateX(-50%) rotate(3600deg) scaleX(1);
+    bottom: 30px;
+    left: 50%;
+  }
+  100% {
+    transform: translateX(-50%) scaleX(0);
+    bottom: 30px;
+    left: 50%;
+  }
+}
+</style>
