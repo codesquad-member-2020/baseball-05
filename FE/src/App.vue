925c93eb5cf60228fe7cb9d0f08534528c03b32c
Hu2y <sanghun_@daum.net>
2020-05-11 22:06:06 +0900

    [#43] Chore: App컴포넌트 불필요한 css 코드 정리

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -10,18 +10,11 @@ export default {};
 
 <style>
 body {
-  /* background-image: url('./assets/bg.jpg');
-  background-color: #41b883;
-  background-repeat: no-repeat;
-  background-size: cover;
-  color: #fff; */
-}
-body {
   margin: 0;
+  overflow: hidden;
 }
 #app {
   background: url('https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Angels_Stadium.JPG/1200px-Angels_Stadium.JPG');
-  /* center no-repeat; */
   background-size: cover;
   background-repeat: no-repeat;
   min-height: 100vh;
