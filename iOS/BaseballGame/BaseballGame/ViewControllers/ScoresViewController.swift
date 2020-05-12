a4a17b1d133145ad93ec25246f67e1d086ab1a93
ehgud0670 <kimdo2297@gmail.com>
2020-05-12 13:25:20 +0900

    [#36] feat: toggle 방식을 이용하여 CurrentViweModels의 데이터가 awayTeam 이 되거나, 	    homeTeam 이 되도록 기능 구현함

--- iOS/BaseballGame/BaseballGame/ViewControllers/ScoresViewController.swift
+++ iOS/BaseballGame/BaseballGame/ViewControllers/ScoresViewController.swift
@@ -11,15 +11,21 @@ import UIKit
 final class ScoresViewController: UIViewController {
     @IBOutlet weak var titleView: TitleView!
     @IBOutlet weak var playerInfoTable: UITableView!
-    private let playerViewModels = PlayerInfoViewModels(playerInfos: [PlayerInfo(player: "류현진",
-                                                                                 mounts: 1,
-                                                                                 hits: 0,
-                                                                                 outs: 1,
-                                                                                 average: 1.0)])
+    private let playerInfoViewModels = PlayerInfoViewModels(awayTeamPlayerInfos: [PlayerInfo(player: "DUMMY_AWAY",
+                                                                                             mounts: 1,
+                                                                                             hits: 0,
+                                                                                             outs: 1,
+                                                                                             average: 1.0)],
+                                                            homeTeamPlayerInfos: [PlayerInfo(player: "DUMMY_HOME",
+                                                                                             mounts: 1,
+                                                                                             hits: 0,
+                                                                                             outs: 1,
+                                                                                             average: 1.0)])
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTitleView()
+        configureObserver()
         configurePlayerInfoTableDataSource()
     }
     
@@ -27,9 +33,27 @@ final class ScoresViewController: UIViewController {
         titleView.delegate = self
     }
     
-    private func configurePlayerInfoTableDataSource() {
-        playerInfoTable.dataSource = playerViewModels
+    private func configureObserver() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateTableView),
+                                               name: PlayerInfoViewModels.Notification.currentTeamPlayerViewModelsDidChange,
+                                               object: playerInfoViewModels)
     }
+    
+    @objc private func updateTableView() {
+        DispatchQueue.main.async {
+            self.playerInfoTable.reloadData()
+        }
+    }
+    
+    private func configurePlayerInfoTableDataSource() {
+        playerInfoTable.dataSource = playerInfoViewModels
+    }
+    
+    @IBAction func teamSegmentDidTouch(_ sender: UISegmentedControl) {
+        playerInfoViewModels.togglePlayerInfoViewModel()
+    }
+    
 }
 
 extension ScoresViewController: TitleViewDelegate {
--- iOS/BaseballGame/BaseballGame/ViewModels/Score/PlayerInfoViewModels.swift
+++ iOS/BaseballGame/BaseballGame/ViewModels/Score/PlayerInfoViewModels.swift
@@ -9,21 +9,44 @@
 import UIKit
 
 final class PlayerInfoViewModels: NSObject {
-    private let playerViewModels: [PlayerInfoViewModel]
+    enum Notification {
+        static let currentTeamPlayerViewModelsDidChange = Foundation.Notification.Name("currentTeamPlayerViewModelsDidChange")
+    }
+    private let awayTeamPlayerViewModels: [PlayerInfoViewModel]
+    private let homeTeamPlayerViewModels: [PlayerInfoViewModel]
+    private var currentTeamPlayerViewModels: [PlayerInfoViewModel]
+    private var isAwayTeam = false
     
-    init(playerInfos: [PlayerInfo]) {
-        self.playerViewModels = playerInfos.map { PlayerInfoViewModel(playerInfo: $0)}
+    init(awayTeamPlayerInfos: [PlayerInfo], homeTeamPlayerInfos: [PlayerInfo]) {
+        self.awayTeamPlayerViewModels = awayTeamPlayerInfos.map { PlayerInfoViewModel(playerInfo: $0)}
+        self.homeTeamPlayerViewModels = homeTeamPlayerInfos.map { PlayerInfoViewModel(playerInfo: $0)}
+        self.currentTeamPlayerViewModels = self.awayTeamPlayerViewModels
+        self.isAwayTeam = true
+        super.init()
+        NotificationCenter.default.post(name: Notification.currentTeamPlayerViewModelsDidChange,
+                                        object: self)
     }
     
     private func playerInfoViewModel(at index: Int) -> PlayerInfoViewModel? {
-        guard index < playerViewModels.count else { return nil }
-        return playerViewModels[index]
+        guard index < currentTeamPlayerViewModels.count else { return nil }
+        return currentTeamPlayerViewModels[index]
+    }
+    
+    func togglePlayerInfoViewModel() {
+        isAwayTeam = !isAwayTeam
+        changeCurrentViewModel()
+    }
+    
+    func changeCurrentViewModel() {
+        currentTeamPlayerViewModels = isAwayTeam ? awayTeamPlayerViewModels : homeTeamPlayerViewModels
+        NotificationCenter.default.post(name: Notification.currentTeamPlayerViewModelsDidChange,
+                                        object: self)
     }
 }
 
 extension PlayerInfoViewModels: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return playerViewModels.count
+        return currentTeamPlayerViewModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
