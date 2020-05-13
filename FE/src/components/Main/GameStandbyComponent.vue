6056b1e5c3f049147e44f5b8f504bcb13f3e6c41
Hu2y <sanghun_@daum.net>
2020-05-14 01:00:30 +0900

    [#53] Feat: 게임 대기상태 컴포넌트 스타일링

--- FE/src/components/Main/GameStandbyComponent.vue
+++ FE/src/components/Main/GameStandbyComponent.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
-    대기상태
+    <div class="loading-container">
+      <loading-spinner></loading-spinner>
+      <span>상대방이 들어오기를 기다리는 중입니다...</span>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import LoadingSpinner from '@/components/common/LoadingSpinner';
+
+export default {
+  created() {
+    // /api/start를 setTimeout으로 계속 요청 보내야 함
+  },
+  components: {
+    LoadingSpinner,
+  },
+};
 </script>
 
-<style></style>
+<style>
+#app {
+  background: #000;
+}
+
+.loading-container {
+  color: red;
+  /* border: 1px solid #ccc; */
+  /* background-color: #00000080; */
+  border-radius: 3px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  margin: 0 auto;
+  /* width: 600px; */
+}
+</style>
