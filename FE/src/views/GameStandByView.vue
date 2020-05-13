b9315f1aa85c5c3c804f7dabe43c8d8d93752df8
Hu2y <sanghun_@daum.net>
2020-05-13 19:54:02 +0900

    [#53] Feat: standby View파일 생성

---
+++ FE/src/views/GameStandByView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <game-standby-component></game-standby-component>
+  </div>
+</template>
+
+<script>
+import GameStandByComponent from '@/components/Main/GameStandbyComponent';
+
+export default {
+  components: {
+    GameStandbyComponent,
+  },
+};
+</script>
+
+<style></style>
