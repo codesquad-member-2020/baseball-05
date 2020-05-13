893dc608434db801405e6eeddd1cf52e1e661c23
Hu2y <sanghun_@daum.net>
2020-05-13 17:37:30 +0900

    [#43] Feat: 배경쪽에 스코어 영역을 추가해 주었다

---
+++ FE/src/components/ScoreComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="!propsData.homeTeam"></div>
+  <div v-else>
+    <div class="home-score info">{{ propsData.homeTeam.score }}</div>
+    <div class="inning info">{{ propsData.inning.id }}</div>
+    <div class="away-score info">{{ propsData.awayTeam.score }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['propsData'],
+};
+</script>
+
+<style scoped>
+.info {
+  color: gold;
+  position: absolute;
+  font-size: 14px;
+}
+
+.home-score {
+  top: 118px;
+  left: 569px;
+}
+
+.inning {
+  top: 118px;
+  left: 601px;
+}
+
+.away-score {
+  top: 118px;
+  right: 558px;
+}
+</style>
