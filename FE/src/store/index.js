46138c7ddd919c344f3a2b2e0d615fa70a8b6a9c
Hu2y <sanghun_@daum.net>
2020-05-14 11:53:46 +0900

    [#53] Feat: 날아가는 볼 상태값을 Store에 추가

--- FE/src/store/index.js
+++ FE/src/store/index.js
@@ -6,6 +6,8 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     isOffense: true,
+    throwBall: false,
+    flyBall: false,
     currentBatter: true,
     basePlayers: [],
     matchesList: [],
