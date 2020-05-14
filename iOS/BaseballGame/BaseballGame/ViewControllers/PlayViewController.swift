6a431976aa7221356a28770fc421d7b173d73218
ehgud0670 <kimdo2297@gmail.com>
2020-05-14 16:26:47 +0900

    [#47] chore: 실제 서버로부터 데이터가 잘들어오는지 확인함

--- iOS/BaseballGame/BaseballGame/ViewControllers/PlayViewController.swift
+++ iOS/BaseballGame/BaseballGame/ViewControllers/PlayViewController.swift
@@ -54,8 +54,9 @@ final class PlayViewController: UIViewController {
     
     private func configureUseCase() {
         guard let roomID = roomID else { return }
-        PlayUseCase.reqeustPlayData(from: PlayUseCase.PlayDataRequest(matchID: roomID), with: PlayUseCase.PlayDataTask(networkDispatcher: MockRoundsSuccess())) { playDataResponse in
-            
+        PlayUseCase.reqeustPlayData(from: PlayUseCase.PlayDataRequest(matchID: roomID), with: PlayUseCase.PlayDataTask(networkDispatcher: NetworkManager())) { playDataResponse in
+            guard let playDataResponse = playDataResponse else { return }
+            print(playDataResponse)
         }
     }
     
