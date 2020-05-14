350f0f3dee035205fe720f80eee8a22aeda4c686
Hu2y <sanghun_@daum.net>
2020-05-14 20:28:37 +0900

    [#53] Feat: vue-meterial 라이브러리 설치

--- FE/src/main.js
+++ FE/src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue';
 import router from '@/routes/index';
 import store from '@/store/index';
 import VueConfetti from 'vue-confetti';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
 
+Vue.use(VueMaterial);
 Vue.config.productionTip = false;
 Vue.use(VueConfetti);
 
