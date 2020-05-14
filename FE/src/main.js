3929a9022b9d351f30a2e06b809da53e1db9305e
Hu2y <sanghun_@daum.net>
2020-05-14 11:50:59 +0900

    [#53] Feat: vue-confetti 라이브러리 설치 및 인스턴스 주입

--- FE/src/main.js
+++ FE/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from '@/routes/index';
 import store from '@/store/index';
+import VueConfetti from 'vue-confetti';
 
 Vue.config.productionTip = false;
+Vue.use(VueConfetti);
 
 new Vue({
+  VueConfetti,
   router,
   store,
   render: h => h(App),
