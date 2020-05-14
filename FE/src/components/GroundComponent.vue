b40979324649b14c4e6d699623ee873ed0cff13a
Hu2y <sanghun_@daum.net>
2020-05-14 11:55:20 +0900

    [#53] Feat: Ball 컴포넌트 연결 및 keyframe 스타일 바인딩

--- FE/src/components/GroundComponent.vue
+++ FE/src/components/GroundComponent.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="ground">
+    <ball :class="{ start: throwBall, fly: flyBall }" />
     <player class="default" :class="{ active: this.$store.state.waitBatter }" />
     <div class="ground-wrap" v-for="player in basePlayers" :key="player.index">
       <player :class="player" />
@@ -9,6 +10,7 @@
 
 <script>
 import Player from '@/components/Player';
+import Ball from '@/components/Ball';
 import { mapState } from 'vuex';
 
 export default {
@@ -17,11 +19,12 @@ export default {
   },
 
   computed: {
-    ...mapState(['basePlayers', 'currentBatter']),
+    ...mapState(['basePlayers', 'currentBatter', 'throwBall', 'flyBall']),
   },
 
   components: {
     Player,
+    Ball,
   },
 };
 </script>
@@ -33,5 +36,32 @@ export default {
 
 .active {
   display: none;
+}
+
+.start {
+  animation-name: start;
+}
+
+.fly {
+  animation-name: fly;
+}
+
+@keyframes fly {
+  0% {
+    top: 91%;
+  }
+  100% {
+    right: 37%;
+    top: 32%;
+  }
+}
+
+@keyframes start {
+  0% {
+    top: 66%;
+  }
+  100% {
+    top: 91%;
+  }
 }
 </style>
