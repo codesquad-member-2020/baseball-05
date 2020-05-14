8ab2619bfbea8c81f73fabaab9f52faf32c5e8a4
Hu2y <sanghun_@daum.net>
2020-05-14 11:53:03 +0900

    [#53] Feat: Ball 컴포넌트 추가

---
+++ FE/src/components/Ball.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ballImg" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.ballImg {
+  position: absolute;
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  background-color: #fff;
+  right: 49%;
+  top: 66%;
+  z-index: 1;
+  animation-duration: 1.3s;
+  animation-fill-mode: both;
+}
+
+/* .end {
+  animation-name: end;
+} */
+/*
+@keyframes end {
+  0% {
+  }
+  100% {
+  }
+} */
+</style>
