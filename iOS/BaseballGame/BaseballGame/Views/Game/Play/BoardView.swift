fed33e69c82198e14aa7bd15ab7bc03f30f76bfc
ehgud0670 <kimdo2297@gmail.com>
2020-05-15 12:59:54 +0900

    [#65] refactor: 기존의 pitch 버튼이 가려져서 코드로 구현해서 추가함

--- iOS/BaseballGame/BaseballGame/Views/Game/Play/BoardView.swift
+++ iOS/BaseballGame/BaseballGame/Views/Game/Play/BoardView.swift
@@ -13,20 +13,22 @@ final class BoardView: UIView, WithXib {
     @IBOutlet weak var inningLabel: BoardLabel!
     @IBOutlet weak var halfLabel: BoardLabel!
     @IBOutlet weak var offenseOrDefense: BoardLabel!
-    @IBOutlet weak var pitchButton: PitchButton!
     
     private let groundView = GroundView()
+    let pitchButton = PitchButton()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         insertXibView()
         configurGroundView()
+        configurePitchButton()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         insertXibView()
         configurGroundView()
+        configurePitchButton()
     }
     
     private func configurGroundView() {
@@ -37,6 +39,15 @@ final class BoardView: UIView, WithXib {
         groundView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5).isActive = true
         groundView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         groundView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    }
+    
+    private func configurePitchButton() {
+        addSubview(pitchButton)
+        
+        pitchButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        pitchButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        pitchButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.3).isActive = true
+        pitchButton.heightAnchor.constraint(equalTo: pitchButton.widthAnchor, multiplier: 26/105).isActive = true
     }
     
     func configureSBOsView(sbo: SBO) {
--- iOS/BaseballGame/BaseballGame/Views/Game/Play/PitchButton.swift
+++ iOS/BaseballGame/BaseballGame/Views/Game/Play/PitchButton.swift
@@ -11,6 +11,7 @@ import UIKit
 final class PitchButton: UIButton {
     override init(frame: CGRect) {
         super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false 
         configure()
     }
     
@@ -21,6 +22,11 @@ final class PitchButton: UIButton {
     
     private func configure() {
         layer.cornerRadius = 5
+        backgroundColor = .black
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        titleLabel?.textColor = .white
+        setTitle("PITCH", for: .normal)
+        setTitleColor(titleLabel?.textColor.withAlphaComponent(0.5), for: .highlighted)
         isHidden = true
     }
 }
